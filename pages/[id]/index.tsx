ba8778c7ae728a94c68b5b9d9ea0e01e8bfd9655
Cooper Weiser <cweiser44@gmail.com>
2020-12-17 22:28:08 -0800

    fixed paths

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -52,6 +52,16 @@ export async function getStaticPaths() {
       { params: { id: "4" } },
       { params: { id: "5" } },
       { params: { id: "6" } },
+      { params: { id: "7" } },
+      { params: { id: "8" } },
+      { params: { id: "9" } },
+      { params: { id: "10" } },
+      { params: { id: "11" } },
+      { params: { id: "12" } },
+      { params: { id: "13" } },
+      { params: { id: "14" } },
+      { params: { id: "15" } },
+      { params: { id: "16" } },
     ],
     fallback: false, // See the "fallback" section below
   };
